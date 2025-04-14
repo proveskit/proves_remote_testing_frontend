676fc84feb59aa40b7a799d038072fcf8a28c714
aychar <58487401+hrfarmer@users.noreply.github.com>
2025-04-14 18:23:34 -0500

    use provided aws credentials

--- src/data/s3.ts
+++ src/data/s3.ts
@@ -1,12 +1,16 @@
 import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 
 export const s3Client = new S3Client({
-  region: process.env.AWS_REGION,
+  region: process.env.AWS_REGION ?? "us-west-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "",
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? "",
+  },
 });
 
 export async function getS3LogsObject(key: string) {
   const command = new GetObjectCommand({
-    Bucket: process.env.AWS_BUCKET_NAME,
+    Bucket: process.env.AWS_BUCKET_NAME ?? "",
     Key: key,
   });
 
